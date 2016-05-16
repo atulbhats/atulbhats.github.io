d50f829c06fbfc9b2f1227758f318a3f8be68306
AtulBhatS <mail@atulbhats.com>
2016-05-16 13:55:21 +0530

    Added Demos for Repositories
    Added Terminal, GUI and CV Demos

---
+++ gui/js/js.js
@@ -0,0 +1,365 @@
+function clear_chat(){
+	$('.chat-window').html("");
+}
+
+$(document).ready(function(){
+
+	function submit_message(){
+		$content=$('.message').text();
+		if($content!='Message' && $.trim($content)!=''){
+			$html='<div class="ping"><div class="ping-out"><span class="message-content">'+$content+'</span><span class="other"><span class="timestamp">'+gettime()+'</span><span class="fa-check"></span><span class="fa-check"></span></span></div></div>';
+			$('.chat-window').append($html);
+			$('.message').text("");
+			$('.message').text('Message');
+			$('.message').css('color','#999');
+			$(document).scrollTop($(document).height());	
+			if(parseInt($start)>4){
+				runcommand($content);	
+			}
+		}
+	}
+
+	function sendmessage(){
+		/* add ajax code to send msg here */
+		animate_output("Message Sent");
+	}
+
+	$header=$('#header').css('height');
+	$typer=parseInt($('.type-window').css('height'))-30;
+	$winheight=parseInt($(window).height())-(parseInt($typer));
+	$('.chat-window').css({
+		'margin-bottom':$typer,
+		'min-height': $winheight,
+		'bottom': '-'+$winheight,
+		'padding-bottom':'30px'
+	});
+
+	$sender=$('.send').css('width');
+	$msgwidth=parseInt($(window).width())-(parseInt($sender))-110;
+	$winwidth=parseInt($(window).width());
+	$('.message').css({
+		'width':$msgwidth
+	});
+
+
+	$(window).resize(function(){
+		$sender=$('.send').css('width');
+		$msgwidth=parseInt($(window).width())-(parseInt($sender))-110;
+		$winwidth=parseInt($(window).width());
+		$('.message').css({
+			'width':$msgwidth
+		});
+	});
+
+	function pad(n, width, z) {
+	  z = z || '0';
+	  n = n + '';
+	  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+	}
+
+	function gettime(){
+		$d = new Date();
+		$h=$d.getHours();
+		$m=$d.getMinutes();
+		if($h==12){
+			$a="PM";
+		}
+		else if($h==0){
+			$h=12;
+			$a="AM";
+		}
+		else if($h>0 && $h <12){
+			$a="AM";
+		}
+		else{
+			$a="PM";
+			$h=$h-12;
+		}
+		
+		$h=pad($h,2);
+		$m=pad($m,2);
+
+		return $h+':'+$m+' '+$a;
+	}
+	
+	$welcome1='Hello There';
+
+	$welcome2='Welcome to the GUI Interface. Yes this looks like a chat messenger. <br> You can use the message input at the bottom to look for queries. <br><br>Available Commands:<br><b>Bio</b>, <b>Email</b>, <b>Facebook</b>, <b>Twitter</b>, <b>Instagram</b>, <b>Message</b>, <br><br>And at any time, you can use \'<b>Help</b>\' for the list of available queries.';
+
+	$welcome3='Also, remember you \'keyboarders\', \'<b>Enter</b>\' is new line. \'<b>Ctrl+Enter</b>\' is send.<br>- Atul <span class="emoji emoji-spock"></span><span class="emoji emoji-tongue"></span>';
+
+	$welcome4='Visit the respository <a href=\"http://github.com/atulbhats/atulbhats.com-gui-version';
+
+	$help="<table>"+
+		"<tr><td colspan='2'>Use these Identifiers as pings.</td></tr>"+
+		"<tr><td><b>bio</b></td><td>Displays my bio</td></tr>"+
+		"<tr><td><b>facebook</b></td><td>Gives a link to my facebook profile. <b>fb</b> will also work</td></tr>"+
+		"<tr><td><b>twitter</b></td><td>Gives a link to my twitter profile. <b>tw</b> will also work</td></tr>"+
+		"<tr><td><b>instagram</b></td><td>Gives a link to my instagram photos. <b>ig</b> will also work</td></tr>"+
+		"<tr><td><b>linkedin</b></td><td>Gives a link to my linkedin profile. <b>in</b> will also work</td></tr>"+
+		"<tr><td><b>email</b></td><td>Displays Email Address</td></tr>"+
+		"<tr><td><b>ping</b></td><td>Guides you through the process to send me a message.<b>send message</b> will also work</td></tr>"+
+		
+		"<tr><td><b>exit</b></td><td>Exits Chat.</td></tr>"+
+		"</table>";
+
+	$msg=0;
+	$img=0;
+	$unknown=0;
+	$start=1;
+	var nameReg = /^[a-zA-Z0-9\s\+\-]+$/;
+    var numReg =  /^[0-9\+\-]+$/;
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+
+    setTimeout(function(){
+    	animate_output($welcome1);
+    	setTimeout(function(){
+	    	animate_output($welcome2);
+	    	setTimeout(function(){
+	    		animate_output($welcome3);
+	    		setTimeout(function(){
+	    			animate_output($welcome4);
+	    		},500);
+	    	},500);
+	    },500);
+    },500);
+	
+
+	$('.message').focus(function(){
+		if($(this).text()=='Message'){
+			$(this).text('');
+			$(this).css('color','#000');
+		}
+	});
+
+	$('.message').blur(function(){
+		$text=$(this).text();
+		if($.trim($text)==''){
+			$(this).text('Message');
+			$(this).css('color','#999');
+		}
+	});
+
+	$('.send').click(function(){
+		submit_message();
+		$inputfocus=0;
+	});
+
+	$inputfocus=0;
+
+	$(document).bind('keyup', function(e) {
+		if(e.ctrlKey && (e.which == 13)) {
+			$('.message').blur();
+			submit_message();
+			$inputfocus=0;
+		}
+		else if(e.which==9){
+			$('.message').focus();
+		}
+		else{
+			if($inputfocus==0){
+				return false;
+			}
+		}
+	});
+
+	$('.message').focus(function(){
+		$inputfocus=1;
+	});
+
+	$('.message').blur(function(){
+		$inputfocus=0;
+	});
+	String.prototype.capitalize = function() {
+	    return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+
+	function runcommand(command){
+		command=command.toLowerCase();
+		
+		if($msg>0){
+			if(command=='cancel'){
+				$content='Message sending cancelled.<span class="emoji emoji-upset"></span>';
+				$msg=0;
+			}
+			else if($msg==1){
+				if(nameReg.test(command) && ($.trim(command)!="" )){
+					$name=command;
+					$content="Hello "+$name.capitalize()+",<br> Now please enter your email id, so that I can contact you";
+					$msg=2;
+				}
+				else{
+					$content='Wow Wow, What sort of a name is that? <span class="emoji emoji-doubt"></span>Try again';
+				}
+			}
+			else if($msg==2){
+				if(emailReg.test(command) && ($.trim(command)!="" )){
+					$email=command;
+					$content="Sweet! Would you Like to provide your Number? (yes/no)";
+				}
+				else if(command=='yes'){
+					$content='Awesome! <span class="emoji emoji-smile"></span>. Please enter your Mobile Number. Prefixes, Suffixes Included.';
+					$msg=3;
+				}
+				else if(command=='no'){
+					$content='It\'s ok. <span class="emoji emoji-smile"></span>. Now type in the message you have for me.';
+					$number="Not Provided";
+					$msg=4;
+				}
+				else{
+					$content='Is that really your email? Please check again. <span class="emoji emoji-uncertain"></span>';
+				}
+			}
+			else if($msg==3){
+				if(numReg.test(command) && ($.trim(command)!="" )){
+					$number=command;
+					$content="Thanks! Now type in the message you have for me.";
+					$msg=4;
+				}
+				else{
+					$content='Your number does not seem to be right <span class="emoji emoji-uncertain"></span>';
+				}
+			}
+			else if($msg==4){
+				if($.trim(command)!=""){
+					$message=command;
+					$content='Yay! Your Message has been sent. <span class="emoji emoji-success"></span>';
+					sendmessage();
+					$msg=0;
+				}
+				else{
+					$content='Nothing to tell me? At say Hi! <span class="emoji emoji-upset"></span>';
+				}
+			}
+		}
+		else{
+			switch(command){
+
+				case 'hi':
+				case 'hola':
+				case 'hello': $content='Hi. <span class="emoji emoji-bye"></span>.';
+				break;
+
+				case 'lol':
+				case 'lmao':
+				case 'rofl':
+				case 'roflmao':
+				case 'lulz':
+				case 'muahaha':
+				case 'hehehe':
+				case 'hahaha':
+				case 'ha ha ha': $content='<span class="emoji emoji-laugh"></span><span class="emoji emoji-laugh"></span><span class="emoji emoji-laugh"></span>. Wait!!!! What are you laughing about <span class="emoji emoji-doubt"></span><span class="emoji emoji-doubt"></span>.';
+				break;
+
+				case 'fb':
+				case 'facebook': $content='The Admin has not updated facebook profile link.';
+				break;
+
+				case 'tw':
+				case 'twitter': $content='The Admin has not updated twitter profile link.';
+				break;
+
+				case 'in':
+				case 'linkedin' : $content='The Admin has not updated twitter profile link.';
+
+				break;
+
+				case 'pic': $content='The Admin has not updated image link/tag.';
+							$img=1;
+				break;
+				
+				case 'bio' : $content='The Admin has not updated bio.<span class="emoji emoji-smile"></span>';
+				break;
+
+				case 'help' : $content=$help;
+				break;
+
+				case 'ping':
+				case 'send message' : $content='Ok. So let\'s get started. Let\'s Begin with your Name. <br>Please enter your Name';
+									  $msg=1;
+				break;
+
+				case 'exit' : $content='Ok then. Ciao. <span class="emoji emoji-bye"></span>.<br>Window will close in 5 seconds. <script>setTimeout(function(){window.open("about:blank","_self");},5000);</script>';
+				break;
+
+				/* Add new commands here */
+
+				
+				default: $content=sortunknown();
+				//'I\'m sorry. I dont know what to reply for that. <span class="emoji emoji-messed"></span>';
+			}
+		}
+
+
+		if($img==1){
+			animate_image($content);
+			$img=0;
+		}
+		else{
+			animate_output($content);	
+		}
+		
+
+		
+	}
+
+	function sortunknown(){
+		$reply="";
+		$slno=parseInt((Math.random() * 10)/2);
+		switch($slno){
+			case 1: $reply="I'm Sorry, I dont understand Human Speaking patterns completely yet! <span class='emoji emoji-messed'></span>";
+			break;
+
+			case 2: $reply="Ok. But I understood none of that. Just a Bot not an A.I! <span class='emoji emoji-messed'></span>";
+			break;
+
+			case 3: $reply="If only I was a human to understand unprogrammed commands! <span class='emoji emoji-doubt'></span>";
+			break;
+
+			case 4: $reply="You know I am a programmed web page right??! <span class='emoji emoji-uncertain'></span>";
+			break;
+
+			case 0: $reply="Oops! Didn't get your there. <span class='emoji emoji-uncertain'></span>";
+			break;
+		}
+		return $reply;
+	}
+
+	function animate_output(content){
+		$('.contact p').text('...typing');
+		setTimeout(function(){
+			$('.ping-out:last-child').find('.fa-check:nth-child(2)').css('visibility','visible');
+			setTimeout(function(){
+				$start++;
+				$('.chat-window').append('<div class="ping"><div class="ping-in"><span class="message-content">'+content+'</span><span class="other"><span class="timestamp">'+gettime()+'</span></span></div></div>');
+				$('.contact p').text('Online');
+				$(document).scrollTop($(document).height());	
+				$('.message').blur();
+			},1000);
+		},500);
+	}
+
+	function animate_image(content){
+		$('.contact p').text('...uploading image');
+		setTimeout(function(){
+			$('.ping-out:last-child').find('.fa-check:nth-child(2)').css('visibility','visible');
+			setTimeout(function(){
+				$('.chat-window').append('<div class="ping-img"><div class="ping-in">'+content+'<span class="other"><span class="timestamp">'+gettime()+'</span></span></div></div>');
+				$('.contact p').text('Online');
+				$(document).scrollTop($(document).height());	
+				$('.message').blur();
+				setTimeout(function(){
+					$('.ping-img:last-child').find('img').css({
+						'-webkit-filter': 'none',
+					    '-o-filter': 'none',
+					    '-moz-filter': 'none',
+					    'filter': 'none',
+					});
+				},500);
+			},1000);
+		},500);
+	}
+
+	
+});
---
+++ js.js
@@ -0,0 +1,374 @@
+/* Atul Bhat : Website Terminal code. 
+/* Author: Atul Bhat
+/* Website: http://atulbhats.com
+/* Updated: May 2016
+*/
+
+
+function hidedefault(){
+	$('#defaultline').hide();
+}
+
+function skipintro($type){
+	$intro_run=0;
+	$introstop=true;
+	$('#introdiv').html("");
+	clearInterval($type);			
+	$p==$intro.length;
+	$intro=$intro.replace(/#/gi,'<br>');
+	$intro=$intro.replace(/~/g,'</b>');
+	$intro=$intro.replace(/\^/g,'<b>');
+	$intro=$intro.replace(/CPY/gi,'<span id="copy">&copy;'+$copy+'</span>');
+	$('#introdiv').html($intro);	
+	$('#defaultline').show();
+	clearInterval($type);		
+}
+
+
+$(document).ready(function(){
+
+	$('#introdiv').html('');
+
+	$time=new Date();
+	$time_hours=$time.getHours();
+	$time_mins=$time.getMinutes();
+
+
+	$z=0;
+	$x=0;
+	$('#actualinput').focus();
+	$('#defaultline').hide();
+	$('.cursor').css('background','rgb(238, 238, 238)');	
+	$link="";
+	$p=0; /*intro counter*/
+	$msg=0;
+	$asciia=0;
+	$msgcmd=0;
+	$sendact=0;
+	$clr=0;
+	$save=0;
+	/*
+	# : br
+	^ : bold open
+	%: 	copyright
+	~ : bold close
+	*/
+	$copy=" 2016 AtulBhatS - terminal version. Hosted by GitHub"; /* Hey you Code Reader. You may Steal the Idea. Copy the code. But dont steal the code */
+	$intro="Welcome to atulbhats.github.io . Designed like a terminal. ##"+
+			"To view any of my repositories, enter the respective names or codes as commands: #"+
+			"^Terminal~ : atulbhats.com-terminal-version #"+
+			"^GUI~ : atulbhats.com-gui-version #"+
+			"^friggeri~ or ^cv~ : Friggeri-CV-HTML-Template #"+
+			"#"+
+			"You can also use: #"+
+			"^website~ or ^atulbhats.com~ : To visit my website www.atulbhats.com #"+
+			"^github~ : To visit my github space github.com/atulbhats #"+
+			"^cls~ : To Clear Screen #"+
+			"##CPY";
+	
+	$intro_run=1;
+	
+
+	$type=setInterval(function(){
+						writeintro();
+					  },15);
+
+	function writeintro(){
+			$html=$('#introdiv').html();
+			if($p<$intro.length-1 && $intro_run==1){
+				if($intro[$p]=='#') $('#introdiv').html($html+'<br>');
+				/*else if($intro[$p]=='^') $('#introdiv').html($html+'<b>');
+				else if($intro[$p]=='~') $('#introdiv').html($html+'</b>');*/
+				else if($intro[$p]=='%') $('#introdiv').html($html+'<span id="copy">&copy;');
+				else if($intro[$p]=='*') $('#introdiv').html($html+'</span>');
+				else if($intro[$p]=='C' && $intro[$p+1]=='P' && $intro[$p+2]=='Y'){
+					$('#introdiv').html($html+'<span id="copy">&copy;'+$copy+'</span>');
+					$p=$p+2;	
+				} 
+				else {
+					$('#introdiv').html($html+$intro[$p]);
+				}
+					
+			}
+			else if($p>=$intro.length-1){
+				clearInterval($type);
+				$intro_run=0;
+				$('#defaultline').show();
+				$loadedintro=$('#introdiv').html();
+				$loadedintro=$loadedintro.replace(/\^/g,'<b>');
+				$loadedintro=$loadedintro.replace(/~/g,'</b>');
+				$('#introdiv').html($loadedintro);
+			}
+			$p++;
+	}
+
+	setInterval(function(){
+					blinkcursor();
+				},560);
+
+
+	function blinkcursor(){
+		$bg=$('.cursor').css('background-color');
+		if($bg=='rgb(238, 238, 238)'){
+			
+			$('.cursor').css('background-color','transparent');	
+		}
+		else $('.cursor').css('background-color','rgb(238, 238, 238)');
+	}
+
+	$l=0;
+	$demo=0;
+	$repo=0;
+
+	var $history=new Array();
+	
+	function runcommand($command){
+		/*addcommand();*/
+		unnull();
+		rehistory($command);
+		$history[$z]=$command;
+		$z++;
+		$x=$z;
+		$command=$command.toLowerCase();
+
+		$('#defaultline').before('<div class="commandline" id="commandline'+$l+'"><span class="defaulttext">user@atulbhtas.github : ~$ </span>'+$command+'</div>');
+		if($command=='exit'){
+			$html="The Site Will Self Destruct in <span id='dest'>5</span>";
+			hidedefault();
+			destroy();		
+		}
+		else{
+			switch($command){
+				case 'repo':
+					if($repo!=0){
+						$html='Loading the respository. Please Wait. <script>setTimeout(function(){window.open("'+$repo+'","_self");},2000);';
+					}
+					else{
+						$html='Project was not loaded correctly. Please type the project name first. And then type repo';
+					}
+				break;
+
+				case 'demo':
+					if($demo!=0){
+						$html='Loading the demo. Please Wait. <script>setTimeout(function(){window.open("'+$demo+'","_self");},2000);';
+					}
+					else{
+						$html='Project was not loaded correctly. Please type the project name first. And then type demo';
+					}
+				break;
+
+				case 'terminal' : 
+				case 'atulbhats.com-terminal-version' :
+					$html='Please enter \'<b>demo</b>\' to view the demo or \'<b>repo</b>\' to view the respository of Terminal Website Code';
+					$demo='http://atulbhats.github.io/terminal';
+					$repo='http://github.com/atulbhats/atulbhats.com-terminal-version';
+				break;
+
+				case 'gui' : 
+				case 'atulbhats.com-gui-version' : 
+					$html='Please enter \'<b>demo</b>\' to view the demo or \'<b>repo</b>\' to view the respository of Terminal Website Code';
+					$demo='http://atulbhats.github.io/gui';
+					$repo='http://github.com/atulbhats/atulbhats.com-gui-version';
+				break;
+				
+				case 'friggeri' : 
+				case 'cv' : 
+				case 'friggeri-cv-html-template' : 
+					$html='Please enter \'<b>demo</b>\' to view the demo or \'<b>repo</b>\' to view the respository of Terminal Website Code';
+					$demo='http://atulbhats.github.io/friggeri';
+					$repo='http://github.com/atulbhats/friggeri-cv-html-template';
+
+				case 'website' : 
+				case 'atulbhats.com' : 
+					$html='Taking you to my website. Please Wait. <script>setTimeout(function(){window.open("http://atulbhats.com","_self");},2000);';
+				break;
+
+				case 'github' : 
+					$html='Taking you to my github space. Please Wait. <script>setTimeout(function(){window.open("http://github.com/atulbhats","_self");},2000);';
+				break;
+
+				case 'hi':
+				case 'hello':
+					$html=$command+" to you too!";
+				break;
+
+				case 'hola':
+					$html="Hola! ¿Como estas? ¿Hablo Engles? ";
+				break;
+				
+				case 'email':
+					$html="You can email me at <b>ping@atulbhats.com</b>";
+					$link="mailto:ping@atulbhats.com";
+					break;
+
+				case 'exit':
+					$html="The Site Will Self Destruct in <span id='dest'>5</span>";
+					destroy();
+				break;
+
+				case 'clr':
+				case 'cls':
+					$clr=1;
+					$msg=0;
+					$html="";
+				break;
+
+				case '':
+				$html="";
+				break;
+
+				case 'reload':
+				$html="<script>location.reload();</script>";
+				break;
+
+				default :
+				$html="\'"+$command+"\' Is not a known Command. But that might change the next time you are here. Use '<b>help</b>' for the list of available commands";
+
+			}
+		}
+
+	
+
+		$('#defaultline').before('<div class="line" id="line'+$l+'"></div>');
+		$('#line'+$l).html($html);
+		$('#commandcontainer').text("");
+		$('#actualinput').val("");
+		$l++;
+		if($clr==1){
+			$l=0;
+			$('.line').remove();
+			$('.commandline').remove();
+			$clr=0;
+		}
+		$('#actualinput').focus();
+		$(document).scrollTop($(document).height());
+
+	}
+
+	$(document).bind('keyup', function(e) {
+		$existing=$('#commandcontainer').text();
+
+		if(e.which==27 && $intro_run==1){
+			skipintro($type);
+		}
+		else if(e.which==38){
+			if($x>0){
+				$('#actualinput').val($history[$x-1]);
+				$('#commandcontainer').text($history[$x-1]);
+				$x--;
+				if($x<0)
+					$x=0;	
+			}
+			if($x<0){
+				return false;
+			}
+			
+		}
+		else if(e.which==40){
+			if($x>=0){
+				$('#commandcontainer').text($history[$x+1]);
+				$('#actualinput').val($history[$x+1]);
+				$x++;
+			}
+			if($x>$z){
+				return false;
+			}
+		}
+		if(e.altKey && (e.which == 83)) {
+		  
+	    /*send msg if $msg==1 which is activated when message command is typed*/
+		    if($msg==1){
+		    	$sendact=1;
+				 runcommand($existing);
+			}
+
+		 /*   return false; */
+		}
+		else if(e.ctrlKey && (e.which == 82)) {
+		 e.preventDefault();
+	    /*send msg if $msg==1 which is activated when message command is typed*/
+		    if($msg==1){
+				if (confirm('Refreshing the Page with Discard Message!!') == true) {
+			        window.reload();
+			    } else {
+			        e.preventDefault();
+			        return false;
+			    }
+			}
+
+		 /*   return false;*/
+		}
+		else if(e.altKey && (e.which == 67)) {
+		  
+	    /*send msg if $msg==1 which is activated when message command is typed*/
+		    if($msg==1){
+		    	$msg=0;
+				 runcommand('cancelsending');
+			}
+
+		 /*  return false;*/
+		}
+		else if(e.which==13){
+			/*enter pressed */
+			/*$key[$k]=$('#commandcontainer').text;*/
+			runcommand($existing);
+			$('#actualinput').focus();
+		}
+		else{
+			$type=true;
+		}
+		/*else if(e.which==38){
+			$('#commandcontainer').text=$key[$k];
+		}*/
+		});
+
+	$('#actualinput').keyup(function(e){
+		if(e.which==8){
+		  	$exist=$('#commandcontainer').text();
+		  	/*delete pressed */
+		  	e.preventDefault();
+		  	/*alert('del');*/
+		  	$c=$exist.length-1;
+		  	$('#commandcontainer').text($exist.slice(0,$c));
+		}
+	  	else{
+	  		/*alert("pressed : "+e.which);*/
+			$('#commandcontainer').text($(this).val());
+		}
+	});
+
+	/*setInterval(function(){
+					$('#actualinput').focus();
+				},5000);*/
+
+
+	$('body').click(function(){
+		$('body').scrollTop($(window).height());
+	});
+
+	$(document).keydown(function(e) {
+		$('#actualinput').focus();
+	});
+
+	
+	function unnull(){
+		for($i=0;$i<$history.length;$i++){
+			if($history[$i]=="" || $history[$i]==null || $history[$i]==undefined ){
+				for($j=$i;$j<$history.length;$j++){
+					$history[$j]=$history[$j+1];
+				}
+				$z--;
+			}
+		}
+		$z=$history.length;
+	}
+
+	function rehistory($cmd){
+		if($history.indexOf($cmd)>=0){
+			for($i=$history.indexOf($cmd);$i<$history.length;$i++){
+				$history[$i]=$history[$i+1];
+			}
+			$z--;
+		}
+	}	
+});
