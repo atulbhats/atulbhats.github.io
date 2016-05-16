e9dd21c106f206e513a7d93bc1caeeb28dd2c21a
AtulBhatS <mail@atulbhats.com>
2016-05-16 13:59:39 +0530

    Minor Bug Fix

--- js.js
+++ js.js
@@ -177,6 +177,7 @@ $(document).ready(function(){
 					$html='Please enter \'<b>demo</b>\' to view the demo or \'<b>repo</b>\' to view the respository of Terminal Website Code';
 					$demo='http://atulbhats.github.io/friggeri';
 					$repo='http://github.com/atulbhats/friggeri-cv-html-template';
+					break;
 
 				case 'website' : 
 				case 'atulbhats.com' : 
